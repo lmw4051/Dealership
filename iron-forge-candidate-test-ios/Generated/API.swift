4a5c430ad6ae03ddbaeaaf34d9f4ff46564c1a3a
David <dvdlee41@gmail.com>
2020-12-16 21:17:05 +0800

    Parsing GraphQL data

--- iron-forge-candidate-test-ios/Generated/API.swift
+++ iron-forge-candidate-test-ios/Generated/API.swift
@@ -13,6 +13,15 @@ public final class DealershipListQuery: GraphQLQuery {
         __typename
         id
         name
+        address
+        logoUrl
+        vehicles {
+          __typename
+          name
+          address
+          imageUrl
+          priceCentsPerDay
+        }
       }
     }
     """
@@ -25,9 +34,11 @@ public final class DealershipListQuery: GraphQLQuery {
   public struct Data: GraphQLSelectionSet {
     public static let possibleTypes: [String] = ["Query"]
 
-    public static let selections: [GraphQLSelection] = [
-      GraphQLField("dealerships", type: .nonNull(.list(.nonNull(.object(Dealership.selections))))),
-    ]
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("dealerships", type: .nonNull(.list(.nonNull(.object(Dealership.selections))))),
+      ]
+    }
 
     public private(set) var resultMap: ResultMap
 
@@ -51,11 +62,16 @@ public final class DealershipListQuery: GraphQLQuery {
     public struct Dealership: GraphQLSelectionSet {
       public static let possibleTypes: [String] = ["Dealership"]
 
-      public static let selections: [GraphQLSelection] = [
-        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-        GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
-        GraphQLField("name", type: .nonNull(.scalar(String.self))),
-      ]
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          GraphQLField("address", type: .nonNull(.scalar(String.self))),
+          GraphQLField("logoUrl", type: .nonNull(.scalar(String.self))),
+          GraphQLField("vehicles", type: .nonNull(.list(.nonNull(.object(Vehicle.selections))))),
+        ]
+      }
 
       public private(set) var resultMap: ResultMap
 
@@ -63,8 +79,8 @@ public final class DealershipListQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init(id: GraphQLID, name: String) {
-        self.init(unsafeResultMap: ["__typename": "Dealership", "id": id, "name": name])
+      public init(id: GraphQLID, name: String, address: String, logoUrl: String, vehicles: [Vehicle]) {
+        self.init(unsafeResultMap: ["__typename": "Dealership", "id": id, "name": name, "address": address, "logoUrl": logoUrl, "vehicles": vehicles.map { (value: Vehicle) -> ResultMap in value.resultMap }])
       }
 
       public var __typename: String {
@@ -91,6 +107,102 @@ public final class DealershipListQuery: GraphQLQuery {
         }
         set {
           resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+
+      public var address: String {
+        get {
+          return resultMap["address"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "address")
+        }
+      }
+
+      public var logoUrl: String {
+        get {
+          return resultMap["logoUrl"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "logoUrl")
+        }
+      }
+
+      public var vehicles: [Vehicle] {
+        get {
+          return (resultMap["vehicles"] as! [ResultMap]).map { (value: ResultMap) -> Vehicle in Vehicle(unsafeResultMap: value) }
+        }
+        set {
+          resultMap.updateValue(newValue.map { (value: Vehicle) -> ResultMap in value.resultMap }, forKey: "vehicles")
+        }
+      }
+
+      public struct Vehicle: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["Vehicle"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLField("name", type: .nonNull(.scalar(String.self))),
+            GraphQLField("address", type: .nonNull(.scalar(String.self))),
+            GraphQLField("imageUrl", type: .nonNull(.scalar(String.self))),
+            GraphQLField("priceCentsPerDay", type: .nonNull(.scalar(Int.self))),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(name: String, address: String, imageUrl: String, priceCentsPerDay: Int) {
+          self.init(unsafeResultMap: ["__typename": "Vehicle", "name": name, "address": address, "imageUrl": imageUrl, "priceCentsPerDay": priceCentsPerDay])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var name: String {
+          get {
+            return resultMap["name"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+
+        public var address: String {
+          get {
+            return resultMap["address"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "address")
+          }
+        }
+
+        public var imageUrl: String {
+          get {
+            return resultMap["imageUrl"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "imageUrl")
+          }
+        }
+
+        public var priceCentsPerDay: Int {
+          get {
+            return resultMap["priceCentsPerDay"]! as! Int
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "priceCentsPerDay")
+          }
         }
       }
     }
--- iron-forge-candidate-test-ios/ViewControllers/DealershipListViewController.swift
+++ iron-forge-candidate-test-ios/ViewControllers/DealershipListViewController.swift
@@ -8,64 +8,60 @@
 
 import UIKit
 
-
-
-struct Dealership {
-    var id: String
-    var name: String
-}
-
 class DealershipListViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
-    
-    
-    var dealerships: [Dealership] = [] {
-        didSet {
-            tableView.reloadData()
-        }
+  @IBOutlet weak var tableView: UITableView!
+  
+  var dealerships: [Dealership] = [] {
+    didSet {
+      tableView.reloadData()
     }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        // Do any additional setup after loading the view.
-        
-        Apollo.client.fetch(query: DealershipListQuery(), resultHandler: {
-            guard let newDealerships = try? $0.get().data?.dealerships
-                .map({ Dealership(id: $0.id, name: $0.name) }) else {
-                    return
-            }
-            
-            self.dealerships = newDealerships
-        })
-    }
-    
+    loadData()
+  }
+  
+  fileprivate func loadData() {
+    Apollo.client.fetch(query: DealershipListQuery(), resultHandler: {
+      guard let newDealerships = try? $0.get().data?.dealerships
+        .map({ Dealership(
+          id: $0.id,
+          name: $0.name,
+          address: $0.address,
+          logoUrl: $0.logoUrl,
+          vehicles: $0.vehicles.map({ Vehicle(
+            name: $0.name,
+            address: $0.address,
+            imageUrl: $0.imageUrl,
+            priceCentsPerDay: $0.priceCentsPerDay) })) }) else {
+          return
+      }
+
+      self.dealerships = newDealerships
+      print(self.dealerships)
+    })
+  }
 }
-
-
 
 extension DealershipListViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.dealerships.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.dealerships.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "dealershipCell")!
     
+    cell.textLabel?.text = dealerships[indexPath.row].name
     
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "dealershipCell")!
-        
-        cell.textLabel?.text = dealerships[indexPath.row].name
-
-        return cell
-    }
-    
-    
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Segue to dealership detail
-    }
-
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    // Segue to dealership detail
+  }
+  
 }
